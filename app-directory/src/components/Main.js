6de68761e3e3392e2b5cb23c0b6f58800eea249e
jeff-mcc <reffery@gmail.com>
2021-04-22 14:24:02 -0600

    updated App.js and .css and created Main.js

---
+++ app-directory/src/components/Main.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Main extends Component{
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <h1>Hello</h1>
+        )
+    }
+}
+
+export default Main
